35f26cbc312f113102bde9ba721a5cdd293faa34
Andre <andreraizdetudo@gmail.com>
2022-07-01 21:28:11 -0300

    realizei a implementação para ajustar o nível de força dos equipamentos do Robotron

--- js/main.js
+++ js/main.js
@@ -1,36 +1,115 @@
-const robotron = document.querySelector('#robotron');
-const saldacoes = document.querySelector('#saldacoes');
+/*FUNÇÕES RESPONSÁVEIS POR FAZER A MONTAGEM DOS EQUIPAMENTOS DO ROBOTRON*/
 
-robotron.addEventListener('mouseenter',saldacaoRobotron);
+/*PRIMEIRA ABORDAGEM DESENVOLVIDA
+// function manipulaForca(equipamento,ajuste) {
 
-/*FUNÇÃO RESPONSÁVEL POR DAR BOAS VINDAS AOS USUÁRIOS*/ 
-function saldacaoRobotron(evento){
-    saldacoes.innerHTML = "Olá humano!!!";
+// }
+
+
+// function somaForca(equipamento){
+//     let forca = Number(equipamento.value);
+//     equipamento.value = forca + 1;
+// }
+
+
+// function subtraiForca(equipamento){
+//     let forca = Number(equipamento.value);
+//     if(forca > 0){
+//         equipamento.value = forca - 1;
+//     }
+// }
+
+
+// /*AJUSTES DOS EQUIPAMENTOS*/
+// const subtrair = document.querySelector('#subtrair');
+// const somar = document.querySelector('#somar');
+
+
+
+// /*EQUIPAMENTOS ROBOTRON*/
+
+// /*BRAÇOS*/
+// const braco = document.querySelector('#braco #forca');
+
+
+
+// somar.addEventListener("click",()=> {
+//     somaForca(braco);
+// });
+
+// subtrair.addEventListener('click',()=> {
+//     subtraiForca(braco);
+// });
+
+
+
+/******************************************************************** *******************************************************************************************************************************************/
+
+
+/******************************************************************
+    MINHA IMPLEMENTAÇÃO PARA AJUSTAR NÍVEIS DE FORÇA DO ROBOTRON
+ ******************************************************************/
+
+
+
+
+//FUNÇÕES UTILIZADAS PARA AJUSTAR OS NÍVEIS DE FORÇA DOS EQUIPAMENTOS
+
+/** 
+ * FUNÇAO RESPONSÁVEL POR CAPTURAR OS BOTÕES DE AJUSTE DOS SEUS 
+ * RESPECTIVOS EQUIPAMENTOS.
+ */
+function pegaBotoesAjustes(peca) {
+    const botoesAjustes = peca.querySelectorAll('.controle-ajuste');
+    return botoesAjustes;
 }
 
-/*FUNÇÃO RESPONSÁVEL POR FAZER A MONTAGEM DOS BRAÇOS DO ROBOTRON*/ 
+/**
+ * FUNÇÃO RESPONSÁVEL POR CAPTURAR O VALOR DA FORÇA DE SEU RESPECTIVO EQUIPAMENTO
+ */
+function pegaValorForcaPeca(peca) {
+    let valorForcaPeca = peca.childNodes[3].childNodes[3].value;
+    console.log(valorForcaPeca);
+    return valorForcaPeca;
+}
 
-/************************** Meu código ****************************************/
-function controleForca(equipamento,ajuste){
-    
-    if(ajuste.id =='diminuir' && forca <= 0){
-        return
-    }
-    else{
-        if(ajuste.id == 'diminuir'){
-            equipamento.value -=1;
-            return;
-        }
-        equipamento.value+=1;
-    }
-    
+/**
+ * FUNÇÃO RESPONSÁVEL POR AJUSTAR (SOMAR OU SUBTRAIR) A FORÇA DE UM EQUIPAMENTO
+ */
+function ajustaForcaPeca(botoesAjustes, valorForcaPeca) {
+    botoesAjustes.forEach((botao) => {
+        botao.addEventListener('click', (event) => {
+            const controles = event.target
+            const operacao = controles.textContent;
+            if (operacao === '+') {
+                valorForcaPeca = Number(valorForcaPeca) + 1;
+                exibeForca(controles, valorForcaPeca)
+            } else if (operacao === '-' && Number(valorForcaPeca) > 0) {
+                valorForcaPeca = Number(valorForcaPeca) - 1;
+                exibeForca(controles, valorForcaPeca)
+            }
+            
+        });
+    });
 }
 
 
 
+/**
+ * FUNÇÃO RESPONSÁVEL POR EXIBIR A FORÇA DE UM EQUIPAMENTO
+ */
+ function exibeForca(controles, valorForcaPeca) {
+    controles.parentNode.childNodes[3].value = valorForcaPeca;
+}
 
 
+// BUSCANDO OS EQUIPAMENTOS DO ROBOTRON
+const listaPecas = document.querySelectorAll('.peca');
+console.log(listaPecas);
 
-
-
-
+// REALIZANDO OS AJUSTES DE FORÇA EM CADA EQUIPAMENTO DO ROBOTRON
+const pecas = listaPecas.forEach((peca) => {
+    const botoesAjustes = pegaBotoesAjustes(peca);
+    const valorForcaPeca = pegaValorForcaPeca(peca);
+    ajustaForcaPeca(botoesAjustes, valorForcaPeca);
+});
