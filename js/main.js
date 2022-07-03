1314425867503a202373e5752ba7bfb79fc299cb
Andre <andreraizdetudo@gmail.com>
2022-07-03 14:32:01 -0300

    finalizando atualização das estatísticas

--- js/main.js
+++ js/main.js
@@ -1,122 +1,44 @@
-/*FUNÇÕES RESPONSÁVEIS POR FAZER A MONTAGEM DOS EQUIPAMENTOS DO ROBOTRON*/
-
-/*PRIMEIRA ABORDAGEM DESENVOLVIDA
-// function manipulaForca(equipamento,ajuste) {
-
-// }
-
-
-// function somaForca(equipamento){
-//     let forca = Number(equipamento.value);
-//     equipamento.value = forca + 1;
-// }
-
-
-// function subtraiForca(equipamento){
-//     let forca = Number(equipamento.value);
-//     if(forca > 0){
-//         equipamento.value = forca - 1;
-//     }
-// }
-
-
-// /*AJUSTES DOS EQUIPAMENTOS*/
-// const subtrair = document.querySelector('#subtrair');
-// const somar = document.querySelector('#somar');
-
-
-
-// /*EQUIPAMENTOS ROBOTRON*/
-
-// /*BRAÇOS*/
-// const braco = document.querySelector('#braco #forca');
-
-
-
-// somar.addEventListener("click",()=> {
-//     somaForca(braco);
-// });
-
-// subtrair.addEventListener('click',()=> {
-//     subtraiForca(braco);
-// });
-
-
-
-/******************************************************************** *******************************************************************************************************************************************/
-
-
-/******************************************************************
-    MINHA IMPLEMENTAÇÃO PARA AJUSTAR NÍVEIS DE FORÇA DO ROBOTRON
- ******************************************************************/
-
-
-
-
-//FUNÇÕES UTILIZADAS PARA AJUSTAR OS NÍVEIS DE FORÇA DOS EQUIPAMENTOS
-
-/** 
- * FUNÇAO RESPONSÁVEL POR CAPTURAR OS BOTÕES DE AJUSTE DOS SEUS 
- * RESPECTIVOS EQUIPAMENTOS.
- */
-// function pegaBotoesAjustes(peca) {
-//     const botoesAjustes = peca.querySelectorAll('.controle-ajuste');
-//     return botoesAjustes;
-// }
-
-// /**
-//  * FUNÇÃO RESPONSÁVEL POR CAPTURAR O VALOR DA FORÇA DE SEU RESPECTIVO EQUIPAMENTO
-//  */
-// function pegaValorForcaPeca(peca) {
-//     let valorForcaPeca = peca.childNodes[3].childNodes[3].value;
-//     console.log(valorForcaPeca);
-//     return valorForcaPeca;
-// }
-
-// /**
-//  * FUNÇÃO RESPONSÁVEL POR AJUSTAR (SOMAR OU SUBTRAIR) A FORÇA DE UM EQUIPAMENTO
-//  */
-// function ajustaForcaPeca(botoesAjustes, valorForcaPeca) {
-//     botoesAjustes.forEach((botao) => {
-//         botao.addEventListener('click', (event) => {
-//             const controles = event.target
-//             const operacao = controles.textContent;
-//             if (operacao === '+') {
-//                 valorForcaPeca = Number(valorForcaPeca) + 1;
-//                 exibeForca(controles, valorForcaPeca)
-//             } else if (operacao === '-' && Number(valorForcaPeca) > 0) {
-//                 valorForcaPeca = Number(valorForcaPeca) - 1;
-//                 exibeForca(controles, valorForcaPeca)
-//             }
-//         });
-//     });
-// }
-
-// /**
-//  * FUNÇÃO RESPONSÁVEL POR EXIBIR A FORÇA DE UM EQUIPAMENTO
-//  */
-// function exibeForca(controles, valorForcaPeca) {
-//     controles.parentNode.childNodes[3].value = valorForcaPeca;
-// }
-
-
-// // BUSCANDO OS EQUIPAMENTOS DO ROBOTRON
-// const listaPecas = document.querySelectorAll('.peca');
-// console.log(listaPecas);
-
-// // REALIZANDO OS AJUSTES DE FORÇA EM CADA EQUIPAMENTO DO ROBOTRON
-// const pecas = listaPecas.forEach((peca) => {
-//     const botoesAjustes = pegaBotoesAjustes(peca);
-//     const valorForcaPeca = pegaValorForcaPeca(peca);
-//     ajustaForcaPeca(botoesAjustes, valorForcaPeca);
-// });
-
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach((elemento) => {
     elemento.addEventListener('click',(evento) => {
         manipulaDados(evento.target.dataset.controle,evento.target.parentNode);
+        atualizaEstatisticas(evento.target.dataset.peca);
     });
 });
 
@@ -127,4 +49,11 @@ function manipulaDados(operacao,controle){
     }else{
         peca.value = parseInt(peca.value)+1;
     }
+}
+function atualizaEstatisticas(peca) {
+    
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+
 }
