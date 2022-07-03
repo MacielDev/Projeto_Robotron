f7e89963c76c0ca43b338b4fbf208a9e4b8e14a8
Andre <andreraizdetudo@gmail.com>
2022-07-03 11:37:13 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -59,57 +59,72 @@
  * FUNÇAO RESPONSÁVEL POR CAPTURAR OS BOTÕES DE AJUSTE DOS SEUS 
  * RESPECTIVOS EQUIPAMENTOS.
  */
-function pegaBotoesAjustes(peca) {
-    const botoesAjustes = peca.querySelectorAll('.controle-ajuste');
-    return botoesAjustes;
-}
+// function pegaBotoesAjustes(peca) {
+//     const botoesAjustes = peca.querySelectorAll('.controle-ajuste');
+//     return botoesAjustes;
+// }
 
-/**
- * FUNÇÃO RESPONSÁVEL POR CAPTURAR O VALOR DA FORÇA DE SEU RESPECTIVO EQUIPAMENTO
- */
-function pegaValorForcaPeca(peca) {
-    let valorForcaPeca = peca.childNodes[3].childNodes[3].value;
-    console.log(valorForcaPeca);
-    return valorForcaPeca;
-}
+// /**
+//  * FUNÇÃO RESPONSÁVEL POR CAPTURAR O VALOR DA FORÇA DE SEU RESPECTIVO EQUIPAMENTO
+//  */
+// function pegaValorForcaPeca(peca) {
+//     let valorForcaPeca = peca.childNodes[3].childNodes[3].value;
+//     console.log(valorForcaPeca);
+//     return valorForcaPeca;
+// }
 
-/**
- * FUNÇÃO RESPONSÁVEL POR AJUSTAR (SOMAR OU SUBTRAIR) A FORÇA DE UM EQUIPAMENTO
- */
-function ajustaForcaPeca(botoesAjustes, valorForcaPeca) {
-    botoesAjustes.forEach((botao) => {
-        botao.addEventListener('click', (event) => {
-            const controles = event.target
-            const operacao = controles.textContent;
-            if (operacao === '+') {
-                valorForcaPeca = Number(valorForcaPeca) + 1;
-                exibeForca(controles, valorForcaPeca)
-            } else if (operacao === '-' && Number(valorForcaPeca) > 0) {
-                valorForcaPeca = Number(valorForcaPeca) - 1;
-                exibeForca(controles, valorForcaPeca)
-            }
-            
-        });
+// /**
+//  * FUNÇÃO RESPONSÁVEL POR AJUSTAR (SOMAR OU SUBTRAIR) A FORÇA DE UM EQUIPAMENTO
+//  */
+// function ajustaForcaPeca(botoesAjustes, valorForcaPeca) {
+//     botoesAjustes.forEach((botao) => {
+//         botao.addEventListener('click', (event) => {
+//             const controles = event.target
+//             const operacao = controles.textContent;
+//             if (operacao === '+') {
+//                 valorForcaPeca = Number(valorForcaPeca) + 1;
+//                 exibeForca(controles, valorForcaPeca)
+//             } else if (operacao === '-' && Number(valorForcaPeca) > 0) {
+//                 valorForcaPeca = Number(valorForcaPeca) - 1;
+//                 exibeForca(controles, valorForcaPeca)
+//             }
+//         });
+//     });
+// }
+
+// /**
+//  * FUNÇÃO RESPONSÁVEL POR EXIBIR A FORÇA DE UM EQUIPAMENTO
+//  */
+// function exibeForca(controles, valorForcaPeca) {
+//     controles.parentNode.childNodes[3].value = valorForcaPeca;
+// }
+
+
+// // BUSCANDO OS EQUIPAMENTOS DO ROBOTRON
+// const listaPecas = document.querySelectorAll('.peca');
+// console.log(listaPecas);
+
+// // REALIZANDO OS AJUSTES DE FORÇA EM CADA EQUIPAMENTO DO ROBOTRON
+// const pecas = listaPecas.forEach((peca) => {
+//     const botoesAjustes = pegaBotoesAjustes(peca);
+//     const valorForcaPeca = pegaValorForcaPeca(peca);
+//     ajustaForcaPeca(botoesAjustes, valorForcaPeca);
+// });
+
+const controle = document.querySelectorAll("[data-controle]");
+
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click',(evento) => {
+        manipulaDados(evento.target.dataset.controle,evento.target.parentNode);
     });
-}
-
-
-
-/**
- * FUNÇÃO RESPONSÁVEL POR EXIBIR A FORÇA DE UM EQUIPAMENTO
- */
- function exibeForca(controles, valorForcaPeca) {
-    controles.parentNode.childNodes[3].value = valorForcaPeca;
-}
-
-
-// BUSCANDO OS EQUIPAMENTOS DO ROBOTRON
-const listaPecas = document.querySelectorAll('.peca');
-console.log(listaPecas);
-
-// REALIZANDO OS AJUSTES DE FORÇA EM CADA EQUIPAMENTO DO ROBOTRON
-const pecas = listaPecas.forEach((peca) => {
-    const botoesAjustes = pegaBotoesAjustes(peca);
-    const valorForcaPeca = pegaValorForcaPeca(peca);
-    ajustaForcaPeca(botoesAjustes, valorForcaPeca);
 });
+
+function manipulaDados(operacao,controle){
+    const peca = controle.querySelector("[data-contador]");
+    if(operacao == "-"){
+        peca.value = parseInt(peca.value)-1;
+    }else{
+        peca.value = parseInt(peca.value)+1;
+    }
+}
